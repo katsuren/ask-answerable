4a4d0a110881a46c2db15a713a25cb0d675898e4
katsuren <katsuren.com@gmail.com>
2015-04-20 00:38:53 +0900

    回答リンクのリスナー追加

--- src/js/answer.js
+++ src/js/answer.js
@@ -2,7 +2,16 @@ var Bootstrap = {
     authToken: "",
     getAuthenticityToken: function() {
         this.authToken = $("input[name='authenticity_token']").val();
+    },
+
+    addAnswerBoxListener: function() {
+        $(".container-wide").on("click", ".answer-linkBox a[href$='reply']", function(e) {
+            var container = $(this).closest(".questionBox");
+            var askId = container.prop('id').replace("inbox_question_", "");
+            return false;
+        });
     }
 }
 
 Bootstrap.getAuthenticityToken();
+Bootstrap.addAnswerBoxListener();
